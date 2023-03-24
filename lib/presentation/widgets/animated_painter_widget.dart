7d5422d9e13b66efead30b9597dd11e96ceaf2aa
Quiklyz 01 <quiklyz01@Fingresss-Mac-mini.local>
2023-03-24 18:30:17 +0530

    fine tuned the radius animation

--- lib/presentation/widgets/animated_painter_widget.dart
+++ lib/presentation/widgets/animated_painter_widget.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
@@ -5,13 +6,14 @@ class CustomColorPainterAnimated extends StatefulWidget {
   const CustomColorPainterAnimated({super.key});
 
   @override
-  State<CustomColorPainterAnimated> createState() => _CustomAnimatedButtonState();
+  State<CustomColorPainterAnimated> createState() =>
+      _CustomAnimatedButtonState();
 }
 
 class _CustomAnimatedButtonState extends State<CustomColorPainterAnimated>
     with TickerProviderStateMixin {
-  List<Color?> colorsList = [
-    Colors.red[800],
+  List<Color> colorsList = [
+    Colors.red,
     Colors.yellow,
     Colors.cyan,
     Colors.green,
@@ -19,16 +21,19 @@ class _CustomAnimatedButtonState extends State<CustomColorPainterAnimated>
     Colors.orangeAccent,
   ];
   int colorIndex = 0;
+
   late Animation<Color?> _colorAnimation;
   late AnimationController _colorController;
 
-
+  late Animation<double> _radiusAnimation;
+  late AnimationController _radiusController;
 
   bool reverse = false;
   @override
   void initState() {
     initColorAnimation();
-    
+    initRadiusAnimation();
+
     super.initState();
   }
 
@@ -36,25 +41,23 @@ class _CustomAnimatedButtonState extends State<CustomColorPainterAnimated>
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        if (colorIndex!=0) {
-          
-        initColorAnimation();
+        if (colorIndex != 0) {
+          initColorAnimation();
         }
         _colorController.forward();
-     
       },
       child: SizedBox(
         child: CustomPaint(
-          foregroundPainter: CircleProgress(
+          painter: CircleProgress(
             col: _colorAnimation.value!,
+            radi: _radiusAnimation.value,
           ),
           child: Container(
             height: 70,
             width: 70,
-            decoration: const BoxDecoration(
+            decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: Colors.black,
-           
+              color: _colorAnimation.value!.withAlpha(180),
             ),
             child: const Icon(
               Icons.color_lens,
@@ -69,18 +72,15 @@ class _CustomAnimatedButtonState extends State<CustomColorPainterAnimated>
   @override
   void dispose() {
     _colorController.dispose();
-
+    _radiusController.dispose();
     super.dispose();
   }
 
   void initColorAnimation() {
-    
-    
     _colorController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 1500),
     );
-  
 
     _colorAnimation = ColorTween(
       begin: colorsList[colorIndex],
@@ -93,47 +93,77 @@ class _CustomAnimatedButtonState extends State<CustomColorPainterAnimated>
       });
   }
 
-
-
   void reverseLogic() {
-    
     if (_colorController.isCompleted) {
-      if (colorIndex<=colorsList.length-3) {
-        
-      colorIndex++;
-      
-      }else {
-        colorIndex=0;
+      if (colorIndex <= colorsList.length - 3) {
+        colorIndex++;
+      } else {
+        colorIndex = 0;
         initColorAnimation();
-        
-
-        
       }
-    } 
+    }
+  }
+
+  void initRadiusAnimation() async {
+    _radiusController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 3800),
+      reverseDuration: const Duration(milliseconds: 1000),
+    );
+
+    _radiusAnimation = Tween<double>(
+      begin: 15,
+      end: 75,
+    ).animate(
+      CurvedAnimation(
+        parent: _radiusController,
+        curve: Curves.elasticInOut,
+        reverseCurve: Curves.easeInOutCubicEmphasized,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener(
+        (status) async {
+          if (status == AnimationStatus.completed) {
+            _radiusController.reverse();
+          } else if (status == AnimationStatus.dismissed) {
+            await Future.delayed(
+              const Duration(milliseconds: 800),
+            );
+            _radiusController.forward();
+          }
+        },
+      );
+    await Future.delayed(
+      const Duration(milliseconds: 1000),
+    );
+    _radiusController.forward();
   }
 }
 
 class CircleProgress extends CustomPainter {
   Color col;
+  double radi;
 
   CircleProgress({
     required this.col,
+    required this.radi,
   });
   @override
   void paint(Canvas canvas, Size size) {
     //    paint circle
     Paint circle = Paint()
-      ..strokeWidth = 10
+      ..strokeWidth = 25
       ..color = col
       ..style = PaintingStyle.stroke
-      ..maskFilter= const MaskFilter.blur(BlurStyle.normal, 15);
+      ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 35);
 
     Offset center = Offset(size.width / 2, size.height / 2);
     double radius = 70;
 
-    canvas.drawCircle(center, radius, circle);
-
-    
+    canvas.drawCircle(center, radi, circle);
   }
 
   @override
